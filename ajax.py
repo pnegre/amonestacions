fefbf115d20cac93f3d2a4802030a63124c341a0
Pere <pere.negre@gmail.com>
2010-10-05 18:47:01 +0200

    Modificat ajax perquè retorni també llinatges en majúscula o minúscula

--- ajax.py
+++ ajax.py
@@ -11,7 +11,7 @@ from gestib.models import *
 
 @permission_required('amonestacions.posar_amonestacions')
 def llistaAlumnes(request):
-	als = Alumne.objects.filter(llinatge1__startswith=request.GET.get('l1',''))
+	als = Alumne.objects.filter(llinatge1__istartswith=request.GET.get('l1',''))
 	res = [ { 
 			'id': a.id, 
 			'value': a.llinatge1 + ' ' + a.llinatge2 + ', ' + a.nom + ' [' + a.expedient + ']', 

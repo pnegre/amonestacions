2aafdbab926258840e4f75b5107b82c718217fa8
Pere <pere.negre@gmail.com>
2010-12-07 14:43:13 +0100

    Netejat una mica el codi

--- aux.py
+++ aux.py
@@ -64,20 +64,13 @@ def informesPdf(periode,grup):
 	response = HttpResponse(mimetype='application/pdf')
 	response['Content-Disposition'] = 'attachment; filename=informe.pdf'
 	
-	# Our container for 'Flowable' objects
 	elements = []
-	
-	# A large collection of style sheets pre-made for us
 	styles = getSampleStyleSheet()
-	
-	# A basic document for us to write to 'rl_hello_platypus.pdf'
+	styles['Normal'].fontsize=8
 	doc = SimpleDocTemplate(response, leftMargin=25, rightMargin=25, topMargin=25, bottomMargin=25)
 	doc.pagesize = portrait(A4)
-	
-	styles['Normal'].fontsize=8
-	today = datetime.date.today()
-	strdate = str(today.day) + "/" + str(today.month) + "/" + str(today.year)
-	
+		
+	today = datetime.date.today().strftime('%d/%m/%Y')
 	periode = Periode.objects.get(id=periode)
 	amonList = Amonestacio.objects.filter(dataHora__gt=periode.dt1).filter(dataHora__lt=periode.dt2)
 	if grup != "-1":
@@ -87,30 +80,26 @@ def informesPdf(periode,grup):
 	alumnes = set( [ a.alumne for a in amonList ] )
 	
 	for al in sorted(alumnes, key = lambda a: unicode(a)):
-		par = Paragraph("<b>Es Liceu</b>. Carrer Cabana, 31. 07141, Pont d'Inca, Marratxí<br/>Telèfon: 971 60 09 86. E-MAIL: escola@esliceu.com<br/><br/>",
+		par = Paragraph(u"<b>Es Liceu</b>. Carrer Cabana, 31. 07141, Pont d'Inca, Marratxí<br/>Telèfon: 971 60 09 86. E-MAIL: escola@esliceu.com<br/><br/>",
 			styles['Normal'])
 		elements.append(par)
 		elements.append(Paragraph(unicode(periode.descripcio), styles['Normal']))
-		
 		elements.append(Paragraph(unicode(al), styles['Heading1']))
 		
-		elements.append(Paragraph("Data: " + strdate, styles['Normal']))
+		elements.append(Paragraph("Data: " + today, styles['Normal']))
 		elements.append(Paragraph("Curs: " + unicode(al.grup), styles['Normal']))
 		elements.append(Paragraph("Tutor/a: " + unicode(al.grup.tutor) + "<br/>", styles['Normal']))
 		
 		elements.append(Paragraph(u"Informe d'incidències", styles['Heading2']))
 		
-		amons = Amonestacio.objects.filter(dataHora__gt=periode.dt1).filter(dataHora__lt=periode.dt2).filter(alumne=al).order_by('dataHora')
-		for a in amons:
+		for a in amonList.filter(alumne=al).order_by('dataHora'):
 			elements.append(Paragraph(u"<b>Data:</b> " + a.dataHora.strftime('%d/%m/%Y') + u'<br/>', styles['Normal']))
 			elements.append(Paragraph(u"<b>Professor:</b> " + unicode(a.professor) + u'<br/>', styles['Normal']))
 			elements.append(Paragraph(u"<b>Tipus d'incidència:</b> " + unicode(a.gravetat) + u'<br/>', styles['Normal']))
 			elements.append(Paragraph(u"<b>Descripció:</b> " + a.descripcio + u'<br/><br/>', styles['Normal']))
 		
 		totalpunts = puntsAlumnePeriode(al,periode)
-		
 		elements.append(Paragraph(u"Total punts restants: " + unicode(totalpunts) + u'<br/><br/>', styles['Heading3']))
-		
 		elements.append(PageBreak()) 
 	
 	doc.build(elements)
